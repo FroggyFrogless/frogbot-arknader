a1f675120427f2d0d0e59f58f0b04949be249221
FroggyFrogless <SlimerzKing@gmail.com>
2018-02-17 00:54:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,18 @@ client.on('message', message => {
     if (message.content === 'ark!subscribe') {
     	message.reply('Yeah dont forget to subscribe https://www.youtube.com/channel/UCH7NFZ4nEoLwSAV3x-pwccw');
   	}
+    if (message.content === 'ribbit') {
+    	message.reply('Ribbit :frog:');
+  	}
+    if (message.content === 'frog!help') {
+    	message.reply('Work in Progress');
+  	}
+    if (message.content === 'frog!') {
+    	message.reply('Try >frog!help<');
+  	}
+    if (message.content === 'frog!subscribe') {
+    	message.reply('Yeah dont forget to subscribe https://www.youtube.com/c/froggyfrogarmy');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
